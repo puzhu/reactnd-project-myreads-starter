df623f55eb76be8f392ce8565be5b7ff9c654f5f
puzhu <hari.subhash@gmail.com>
2018-09-12 19:54:30 -0400

    App renders cleanly

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -4,25 +4,66 @@ class BookShelf extends Component {
 
     render() {
         const books = this.props.books;
-        const shelves = [... new Set(books.map(book => book.shelf
+        const rawShelves = [...new Set(books.map(book => book.shelf))] 
+        
+        const cleanShelves = rawShelves.map(shelf => shelf
                 .replace(/([A-Z])/g, (match) => ` ${match}`)
-                .replace(/^./, (match) => match.toUpperCase())))]
-        console.log(shelves, books)
+                .replace(/^./, (match) => match.toUpperCase()))
+        
         return (
         
         <div className="list-books-content">
             <div>
-               {shelves.map(shelf => (
-                <div className="bookshelf">
+               {cleanShelves.map((shelf, i) => {
+                 return (
+                 <div className="bookshelf" key={shelf}>
                     <h2 className="bookshelf-title">{shelf}</h2>
                     <div className="bookshelf-books">
                         <ol className="books-grid">
+                          {
+                            books.filter(book => book.shelf === rawShelves[i])
+                              .map(book => {
+                                return (
+                                    <li key={book.title}>
+                                      <div className="book">
+                                        <div className="book-top">
+                                          <div className="book-cover" style={{ 
+                                            width: 128, 
+                                            height: 193, 
+                                            backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                                            <div className="book-shelf-changer">
+                                              <select>
+                                                <option value="move" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                              </select>
+                                            </div>
+                                          </div>
+                                        <div className="book-title">{book.title}</div>
+                                        <div className="book-authors">{book.authors[0]}</div> 
+                                      </div>
+                                    </li>
+                                )
+                              })
+                          }
                         </ol>
                     </div>
                 </div>
-               ))}
+                )
+              })}
             </div>
-        <div>
+        
+        </div>
+        )
+    }
+
+}
+
+export default BookShelf;
+
+{/* <div>
           <div className="bookshelf">
             <h2 className="bookshelf-title">Currently Reading</h2>
             <div className="bookshelf-books">
@@ -170,10 +211,4 @@ class BookShelf extends Component {
               </ol>
             </div>
           </div>
-        </div>
-      </div>)
-    }
-
-}
-
-export default BookShelf;
+        </div> */}
