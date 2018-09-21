aa851f8d61a9e5b2439c46327e9cf32daa573268
puzhu <hari.subhash@gmail.com>
2018-09-20 21:29:43 -0400

    updated search

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
 
 class SearchBar extends Component {
+  state = {
+    query: "",
+    searchBooks: []
+  }
+  onChangeInput(searchTerm) {
+    if (searchTerm.length < 1) {
+      this.setState(() => ({
+        query: "",
+        searchBooks: []
+      }))
+    } else {
+      BooksAPI.search(searchTerm).then(books => this.setState(() => {
+        return ({
+          query: searchTerm,
+          searchBooks: books
+        })
+      }))
+    }
+
+
+  }
   render() {
+    console.log(this.state)
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>
-            Close
-          </a>
+          <Link to="/" className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -17,11 +39,32 @@ class SearchBar extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author" />
+            <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event) => this.onChangeInput(event.target.value)} />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid" />
+          <ol className="books-grid">
+            {this.state.searchBooks.length > 0 && (this.state.searchBooks.map(book => {
+              return (
+                <li key={book.id}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div
+                        className="book-cover"
+                        style={{
+                          width: 128,
+                          height: 193,
+                          backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                        }}
+                      />
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors[0]}</div>
+                  </div>
+                </li>
+              );
+            }))}
+          </ol>
         </div>
       </div>
     );
